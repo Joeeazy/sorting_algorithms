be8f7c771ba5960ae05e2c761a6c9fb3e5dc3e96
Joeeazy <joeeazy34@gmail.com>
2023-11-21 22:19:55 +0300

    Array Data Structure and Functions

--- print_array.c
+++ print_array.c
@@ -1,17 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "sort.h"
 
 /**
-   * print_array - Prints an array of integers
-    *
-     * @array: The array to be printed
-      * @size: Number of elements in @array
-       */
+  * print_array - Prints an array of integers
+  *
+  * @array: The array to be printed
+  * @size: Number of elements in @array
+  */
 void print_array(const int *array, size_t size)
 {
 	size_t i;
-	i = 0;
 
+	i = 0;
 	while (array && i < size)
 	{
 		if (i > 0)
